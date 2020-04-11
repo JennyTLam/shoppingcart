365ebb2b673495d27098dc1ac4ea99c87115ec13
JennyTLam <jennytlam18@gmail.com>
2020-04-11 12:58:26 -0500

    Static Cart Add

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import 'rbx/index.css'
-import { Button, Container, Column, Card } from 'rbx';
+import { Button, Title, Container, Column, Card, Navbar, Delete, Box} from 'rbx';
+import Drawer from '@material-ui/core/Drawer';
+
+const SignIn = () => {
+  return (
+    <Navbar>
+      <Navbar.Brand>
+        <Navbar.Item href="#">
+          <img
+            src="/data/logo.png"
+            alt=""
+            role="presentation"
+            width="150"
+            height="400"
+          />
+        </Navbar.Item>
+        <Navbar.Burger />
+      </Navbar.Brand>
+      <Navbar.Menu>
+        <Navbar.Segment align="end">
+          <Navbar.Item>
+            <Button.Group>
+              <Button color="primary">
+                <strong>Sign In</strong>
+              </Button>
+            </Button.Group>
+          </Navbar.Item>
+        </Navbar.Segment>
+      </Navbar.Menu>
+    </Navbar> 
+  );
+};
 
 const sizes = { S: 'S', M: 'M', L: 'L', XL: 'XL'};
-
-const SizeSelector = () => (
-  <Button.Group hasAddons>
-    { Object.values(sizes)
-      .map(size =>
-        <Button key={ size }
-          >
-          { size }
-        </Button>
-      )
-    }
-  </Button.Group>
-);
 
 const ShirtSizeSelector = () => (
   <Button.Group hasAddons>
@@ -30,9 +48,27 @@ const ShirtSizeSelector = () => (
   </Button.Group>
 );
 
+const formatPrice = ( price ) => {
+  const result = price.toString().split(".");
+  if (result.length === 1){
+    return result[0] + "." + "00";
+  }
+  else if (result.length === 2){
+    const cents = (result[1].length === 1) ? result[1] + "0" : result[1];
+    return result[0] + "." + cents;
+  }
+  else{
+    console.error("Invalid Price Format");
+  }
+};
+
 const App = () => {
   const [data, setData] = useState({});
+  const [openCart, setOpenCart] = useState(false);
+  const [cart, setCart] = useState([]);
+  
   const products = Object.values(data);
+
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('./data/products.json');
@@ -45,21 +81,74 @@ const App = () => {
   const products_sku = products.filter(product => product.sku);
   const num_products = products_sku.length;
 
-  const formatPrice = ( price ) => {
-    const result = price.toString().split(".");
-    if (result.length === 1){
-      return result[0] + "." + "00";
-    }
-    else if (result.length ===2){
-      const cents = (result[1].length == 1) ? result[1] + "0" : result[1];
-      return result[0] + "." + cents
-    }
-    return result.length
+  const productDict = {};
+  for (let i = 0; i < num_products; i++){
+    productDict[products[i].sku] = products[i];
   }
 
-  const createGrid = () => {
-    let grid = [];
+  const cartAdd = (id) => {
+    setOpenCart(true);
+    let tempCart = cart;
+    let inCart = false;
+    for (let i = 0; i < tempCart.length; i++){
+      if (tempCart[i].itemId === id){
+        tempCart[i].qty += 1;
+        inCart = true;
+        break;
+      }
+    }
+    if (!inCart){
+      tempCart.push(
+        {itemId: id, 
+         qty: 1, 
+         product: productDict[id],
+       })
+    }
+    setCart(tempCart);
+  };
 
+  const ShoppingCart = () => {
+    return (
+      <Container>
+        <Button
+          onClick={() => setOpenCart(true)}> Cart 
+        </Button>
+        <Drawer
+          width="100%"
+          anchor="right"
+          open={openCart}
+        > 
+          <Delete onClick={() => setOpenCart(false)}></Delete>
+          <Box style={{pading:'20px'}}>
+            <Title> My Cart </Title>
+          </Box>
+          <Box style={{width:'400px'}}>
+            <Column.Group>
+              {CartItems()}
+            </Column.Group>
+          </Box>
+          <Box style={{float:'left'}}>
+            <Button> Checkout </Button>
+          </Box>
+        </Drawer>
+      </Container>
+    );
+  };
+
+  const WelcomeBanner = () => {
+    return (
+      <div>
+        <div style={{padding:'10px', display:'inline-block'}}>
+          <Title>Welcome to Classic Tees Online Store</Title>
+        </div>
+        <div style={{padding:'5px', float:'right', display:'inline-block'}}>
+          <ShoppingCart />
+        </div>
+      </div>
+    );
+  };
+
+  const ProductList = ( {num_products, products} ) => {
     let items = [];
     for (let i = 0; i < num_products; i++){
       items.push(
@@ -82,7 +171,7 @@ const App = () => {
                 </b>
               </Card.Footer>
               <br></br>
-              <Button key={products[i].sku}> Add to Cart </Button> 
+              <Button key={products[i].sku} onClick ={() => cartAdd(products[i].sku)}> Add to Cart </Button> 
               <br></br>
               <br></br>
             </Card.Content> 
@@ -91,9 +180,7 @@ const App = () => {
     }
 
     let column = [];
-    let start = 0;
-    let end = 3;
-    for(let i = start; i < num_products; i = i + 4){
+    for(let i = 0; i < num_products; i = i + 4){
       if(i + 3 < num_products){
         column.push(
           <Column size="3" key={products[i].sku}>
@@ -122,20 +209,67 @@ const App = () => {
           </Column>
         )
       }
-  
-    }
 
+    }
     return column
-  }
+  };
+
+  const CartItems = () => {
+    let list = [];
+    let products = cart.map(item => item.product)
+    for (let i = 0; i < products.length; i++){
+      list.push(
+        <Card key={products[i].sku} style={{display:'inline-block', width:'100%'}}>
+          <div>
+            <div style={{float:'left'}}>
+              <Card.Image align="left">
+                <img src={'data/products/' + products[i].sku + '_1.jpg'} width="100px" height="100px"/>
+              </Card.Image>
+            </div>
+            <div style={{float:'center'}}>
+              <Card.Content style={{align:'right'}}>
+                <b>{products[i].title}</b>
+                <br></br>
+                <em>{products[i].style}</em>
+                <br></br>
+                <em>{products[i].description}</em>
+                <br></br>
+                <div>
+                  <div>
+                    <Button onClick={ () => cartAdd(products[i].sku) }> + </Button>
+                    <Button> - </Button>
+                  </div>
+                </div>
+                <div style={{float:'right'}}>
+                    <b>
+                      <br></br>
+                      <p>QTY: 1</p>
+                      <br></br>
+                      {products[i].currencyFormat}
+                      {formatPrice(products[i].price)}
+                    </b>
+                </div>
+              </Card.Content>
+            </div>
+          </div>
+        </Card>
+      )
+    }
+    return (
+      <Column> {list} </Column>
+    );
+  };
+
 
   return (
     <React.Fragment>
       <br></br>
-      <Container fluid="true" align="center"> <SizeSelector /> </Container>
+      <br></br>
+      <WelcomeBanner  />
       <br></br>
       <Container>
         <Column.Group multiline="true">
-          {createGrid()}
+          {ProductList({num_products, products})}
         </Column.Group>
       </Container>
     </React.Fragment>
