b426db00ee9c6022e2c2bda062bc37ac83489401
JennyTLam <jennytlam18@gmail.com>
2020-04-11 22:23:40 -0500

    Add show in-stock Interaction

--- src/App.js
+++ src/App.js
@@ -33,23 +33,10 @@ const SignIn = () => {
   );
 };
 
-const sizes = { S: 'S', M: 'M', L: 'L', XL: 'XL'};
 
-const ShirtSizeSelector = () => (
-  <Button.Group hasAddons>
-    { Object.values(sizes)
-      .map(size =>
-        <Button key={ size }
-          >
-          { size }
-        </Button>
-      )
-    }
-  </Button.Group>
-);
 
 const formatPrice = ( price ) => {
-  const result = price.toFixed(2).toString().split(".");
+  const result = Math.abs(price).toFixed(2).toString().split(".");
   if (result.length === 1){
     return result[0] + "." + "00";
   }
@@ -67,9 +54,12 @@ const App = () => {
   const [openCart, setOpenCart] = useState(false);
   const [cart, setCart] = useState([]);
   const [cartTotal, setCartTotal] = useState(0);
-  
-  const products = Object.values(data);
+  const [inventory, setInventory] = useState({});
+  const [count, setCount] = useState(0);
 
+
+
+  
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('./data/products.json');
@@ -79,53 +69,105 @@ const App = () => {
     fetchProducts();
   }, []);
 
+  useEffect(() => {
+    const fetchInventory = async () => {
+      const response = await fetch('./data/inventory/inventory.json');
+      const json = await response.json();
+      setInventory(json);
+    };
+    fetchInventory();
+  }, []);
+
+  const products = Object.values(data);
   const products_sku = products.filter(product => product.sku);
   const num_products = products_sku.length;
+
+  let copyshirt = {}
+  for(let i = 0; i < num_products; i++){
+    copyshirt[products[i].sku] = null;
+    console.log(copyshirt)
+  }
+
+  const [sizeShirt, setSize] = useState(copyshirt);
+  
+
+
 
   const productDict = {};
   for (let i = 0; i < num_products; i++){
     productDict[products[i].sku] = products[i];
   }
 
-  const cartAdd = (id) => {
+  const available_inventory = Object.keys(inventory);
+  const inv = Object.values(inventory)
+  const small = {}
+  const medium = {}
+  const large = {}
+  const xl = {}
+  for (let i = 0; i < available_inventory.length; i++){
+    small[available_inventory[i]] = inv[i].S
+    medium[available_inventory[i]] = inv[i].M
+    large[available_inventory[i]] = inv[i].L
+    xl[available_inventory[i]] = inv[i].XL
+  }
+
+  const cartAdd = (id, size) => {
+    setCount(count + 1);
     setOpenCart(true);
     let tempCart = cart;
+    let tempInv = inventory;
     let inCart = false;
     for (let i = 0; i < tempCart.length; i++){
-      if (tempCart[i].itemId === id){
-        tempCart[i].qty += 1;
+      if (tempCart[i].itemId === (id + size)){
+        if (tempInv[id][size] > 0){
+          tempCart[i].qty += 1;
+          tempInv[id][size] -= 1;
+          setCartTotal(cartTotal + productDict[id].price);
+        }
         inCart = true;
         break;
       }
     }
-    if (!inCart){
+    if (!inCart && tempInv[id][size] > 0){
       tempCart.push(
-        {itemId: id, 
+        {itemId: id + size, 
          qty: 1, 
          product: productDict[id],
+         buy_size: size
        })
+      tempInv[id][size] -= 1;
+      setCartTotal(cartTotal + productDict[id].price);
     }
     setCart(tempCart);
-    setCartTotal(cartTotal + productDict[id].price);
+    setInventory(tempInv);
   };
 
-  const cartRemove = (id) => {
+  const cartRemove = (id, size) => {
+    setCount(count + 1);
     setOpenCart(true);
     let tempCart = cart;
+    let tempInv = inventory; 
     for (let i = 0; i < tempCart.length; i++){
-      if (tempCart[i].itemId === id){
-        if (tempCart[i].qty === 1){
-          tempCart = tempCart.filter(function(value, index, tempCart){return value.itemId !== id;});
-        }
-        else{
-          tempCart[i].qty = tempCart[i].qty - 1;
-        }
+      if (tempCart[i].itemId === (id + size)){
+        tempCart[i].qty -= 1;
+        tempInv[id][size] += 1;
+        break;
       }
     }
+
+    tempCart = tempCart.filter(item => item.qty > 0)
     setCart(tempCart);
     setCartTotal(cartTotal - productDict[id].price);
+    setInventory(tempInv);
   };
 
+  const changeSize = (id, pick_size) =>{
+    let tempSize = sizeShirt;
+    tempSize[id] = pick_size;
+
+    setSize(tempSize);
+
+  };
   const ShoppingCart = () => {
     return (
       <Container>
@@ -168,6 +210,83 @@ const App = () => {
     );
   };
 
+  const outmsg = (bool, id) =>{
+    console.log(sizeShirt[id])
+    if (bool){
+      return (
+          <p> Out of Stock </p>
+      );
+    }
+    else{
+      return(
+        <Button key={"cart" + id + count} disabled={sizeShirt[id] === null} onClick={() => cartAdd(id, sizeShirt[id])}> Add to Cart </Button>
+      )
+    }
+  };
+
+  const ShirtSizeSelector = (id) => {
+    const sizes = ["S", "M", "L", "XL"];
+    let size_buttons = []
+    let out_of_stock = true
+
+    if (small[id] > 0){
+      size_buttons.push(
+        <Button key={id + "S"} disabled={false} onClick={() => changeSize(id, "S")}> S </Button>
+        )
+      out_of_stock = false
+    }
+    else{
+      size_buttons.push(
+        <Button key={id + "S"} disabled={true}> S </Button>
+      )
+    }
+
+    if (medium[id] > 0){
+      size_buttons.push(
+        <Button key={id + "M"} disabled={false} onClick={() => changeSize(id, "M")}> M </Button>
+        )
+      out_of_stock = false
+    }
+    else{
+      size_buttons.push(
+        <Button key={id + "M"} disabled={true}> M </Button>
+      )
+    }
+
+    if (large[id] > 0){
+      size_buttons.push(
+        <Button key={id + "L"} disabled={false} onClick={() => changeSize(id, "L")}> L </Button>
+        )
+      out_of_stock = false
+    }
+    else{
+      size_buttons.push(
+        <Button key={id + "L"} disabled={true}> L </Button>
+      )
+    }
+
+    if (xl[id] > 0){
+      size_buttons.push(
+        <Button key={id + "XL"} disabled={false} onClick={() => changeSize(id, "XL")}> XL </Button>
+        )
+      out_of_stock = false
+    }
+    else{
+      size_buttons.push(
+        <Button key={id + "XL"} disabled={true}> XL </Button>
+      )
+    }
+
+    return (
+      <React.Fragment>
+        <Button.Group hasAddons>
+          {size_buttons}
+        </Button.Group>
+        <p> {outmsg(out_of_stock, id)} </p>
+      </React.Fragment>
+    );
+  };
+
   const ProductList = ( {num_products, products} ) => {
     let items = [];
     for (let i = 0; i < num_products; i++){
@@ -184,14 +303,13 @@ const App = () => {
               <Card.Footer>
                 <b>
                   <br></br>
-                  <ShirtSizeSelector />
                   {products[i].currencyFormat}
                   {formatPrice(products[i].price)}
                   <br></br>
                 </b>
               </Card.Footer>
               <br></br>
-              <Button key={products[i].sku} onClick ={() => cartAdd(products[i].sku)}> Add to Cart </Button> 
+              {ShirtSizeSelector(products[i].sku)}
               <br></br>
               <br></br>
             </Card.Content> 
@@ -239,7 +357,7 @@ const App = () => {
     let products = cart.map(item => item.product)
     for (let i = 0; i < products.length; i++){
       list.push(
-        <Card key={products[i].sku} style={{display:'inline-block', width:'100%'}}>
+        <Card key={products[i].sku+sizeShirt[products[i].sku]} style={{display:'inline-block', width:'100%'}}>
           <div>
             <div style={{float:'left'}}>
               <Card.Image align="left">
@@ -256,13 +374,14 @@ const App = () => {
                 <br></br>
                 <div>
                   <div>
-                    <Button onClick={ () => cartAdd(products[i].sku) }> + </Button>
-                    <Button onClick={ () => cartRemove(products[i].sku) }> - </Button>
+                    <Button key={"plus" + products[i].sku + cart[i].itemId} disabled={inventory[products[i].sku][cart[i].buy_size] == 0} onClick={ () => cartAdd(products[i].sku, cart[i].buy_size) }> + </Button>
+                    <Button key={"minus" + products[i].sku + cart[i].itemId} onClick={ () => cartRemove(products[i].sku, cart[i].buy_size) }> - </Button>
                   </div>
                 </div>
                 <div style={{float:'right'}}>
                     <b>
                       <br></br>
+                      <p>SIZE: {cart[i].buy_size} </p>
                       <p>QTY: {cart[i].qty} </p>
                       <br></br>
                       {products[i].currencyFormat}
