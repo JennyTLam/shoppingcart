962a62d0ade85d88ba80a41e76bcfbb840b904c8
JennyTLam <jennytlam18@gmail.com>
2020-04-22 00:48:42 -0500

    Added persistent carts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import 'rbx/index.css'
-import { Message, Button, Title, Container, Column, Card, Navbar, Delete, Box} from 'rbx';
+import { Message, Button, Title, Container, Column, Card, Delete, Box} from 'rbx';
 import Drawer from '@material-ui/core/Drawer';
 import firebase from 'firebase/app';
 import 'firebase/database';
@@ -35,7 +35,7 @@ const Welcome = ({ user }) => (
   <Message color="info">
     <Message.Header>
       Welcome, {user.displayName}
-      <Button primary onClick={() => firebase.auth().signOut()}>
+      <Button primary="true" onClick={() => firebase.auth().signOut()}>
         Log out
       </Button>
     </Message.Header>
@@ -71,13 +71,13 @@ const formatPrice = ( price ) => {
 };
 
 const App = () => {
-  const [data, setData] = useState({});
+  const [user, setUser] = useState(null);
   const [openCart, setOpenCart] = useState(false);
+  const [data, setData] = useState({});
+  const [inventory, setInventory] = useState({});
   const [cart, setCart] = useState([]);
   const [cartTotal, setCartTotal] = useState(0);
-  const [inventory, setInventory] = useState({});
   const [count, setCount] = useState(0);
-  const [user, setUser] = useState(null);
   
   useEffect(() => {
     const fetchProducts = async () => {
@@ -86,37 +86,64 @@ const App = () => {
       setData(json);
     };
     fetchProducts();
-  }, []);
-
-  useEffect(() => {
     const handleData = snap => {
-      if (snap.val()) setInventory(snap.val());
+      if (snap.val()) 
+        setInventory(snap.val().inventory);
     };
     db.on('value', handleData, error => alert(error));
     return () => { db.off('value', handleData); };
   }, []);
 
+  const mergeCarts = ({user, pulledCart}) => {
+    const newCart = pulledCart;
+    const fixed = pulledCart;
+    const currentCart = cart;
+    for(let i = 0; i < currentCart.length; i++){
+      let newItem = true;
+      for(let j = 0; j < fixed.length; j++){
+        if (currentCart[i].itemId === newCart[j].itemId){
+          newItem = false;
+          newCart[j].qty += 1;
+          break;
+        }
+      }
+      if (newItem){
+        newCart.push(currentCart[i])
+      }
+    }
+    setCart(newCart)
+    console.log(newCart)
+    db.child('carts').child(user.uid).set(newCart)
+  }
+
+  const userUpdateFunc = (user) => {
+    if(user){
+      const db2 = firebase.database().ref('carts/' + user.uid);
+      const handleData2 = snap => {
+        if(snap.val()){
+          const pulledCart = snap.val()
+          mergeCarts({user, pulledCart});
+          console.log('how')
+        }
+        else{
+          console.log('where')
+          console.log(cart)
+          db.child('carts').child(user.uid).set(cart)
+        }
+        db2.off('value', handleData2);
+      }
+      db2.on('value', handleData2, error => alert(error));
+    }
+    setUser(user);
+  }
+
   useEffect(() => {
-    firebase.auth().onAuthStateChanged(setUser);
+    firebase.auth().onAuthStateChanged(userUpdateFunc);
   }, []);
 
   const products = Object.values(data);
-  const products_sku = products.filter(product => product.sku);
-  const num_products = products_sku.length;
-
-  let copyshirt = {}
-  for(let i = 0; i < num_products; i++){
-    copyshirt[products[i].sku] = null;
-    console.log(copyshirt)
-  }
-
-  const [sizeShirt, setSize] = useState(copyshirt);
-  
-
-
-
   const productDict = {};
-  for (let i = 0; i < num_products; i++){
+  for (let i = 0; i < products.length; i++){
     productDict[products[i].sku] = products[i];
   }
 
@@ -133,92 +160,11 @@ const App = () => {
     xl[available_inventory[i]] = inv[i].XL
   }
 
-  const cartAdd = (id, size) => {
-    setCount(count + 1);
-    setOpenCart(true);
-    let tempCart = cart;
-    let tempInv = inventory;
-    let inCart = false;
-    for (let i = 0; i < tempCart.length; i++){
-      if (tempCart[i].itemId === (id + size)){
-        if (tempInv[id][size] > 0){
-          tempCart[i].qty += 1;
-          tempInv[id][size] -= 1;
-          setCartTotal(cartTotal + productDict[id].price);
-        }
-        inCart = true;
-        break;
-      }
-    }
-    if (!inCart && tempInv[id][size] > 0){
-      tempCart.push(
-        {itemId: id + size, 
-         qty: 1, 
-         product: productDict[id],
-         buy_size: size
-       })
-      tempInv[id][size] -= 1;
-      setCartTotal(cartTotal + productDict[id].price);
-    }
-    setCart(tempCart);
-    setInventory(tempInv);
-  };
-
-  const cartRemove = (id, size) => {
-    setCount(count + 1);
-    setOpenCart(true);
-    let tempCart = cart;
-    let tempInv = inventory; 
-    for (let i = 0; i < tempCart.length; i++){
-      if (tempCart[i].itemId === (id + size)){
-        tempCart[i].qty -= 1;
-        tempInv[id][size] += 1;
-        break;
-      }
-    }
-
-    tempCart = tempCart.filter(item => item.qty > 0)
-    setCart(tempCart);
-    setCartTotal(cartTotal - productDict[id].price);
-    setInventory(tempInv);
-  };
-
-  const changeSize = (id, pick_size) =>{
-    let tempSize = sizeShirt;
-    tempSize[id] = pick_size;
-
-    setSize(tempSize);
-
-  };
-  const ShoppingCart = () => {
-    return (
-      <Container>
-        <Button
-          style={{marginRight:'40px'}}
-          onClick={() => setOpenCart(true)}> <b>Cart </b>
-        </Button>
-        <Drawer
-          width="100%"
-          anchor="right"
-          open={openCart}
-        > 
-          <Delete onClick={() => setOpenCart(false)}></Delete>
-          <Box style={{pading:'20px'}}>
-            <Title> My Cart </Title>
-          </Box>
-          <Box style={{width:'400px'}}>
-            <Column.Group>
-              {CartItems()}
-            </Column.Group>
-          </Box>
-          <Box style={{float:'left'}}>
-            <p> Total: ${formatPrice(cartTotal)} </p>
-            <Button> Checkout </Button>
-          </Box>
-        </Drawer>
-      </Container>
-    );
-  };
+  let copyshirt = {}
+  for(let i = 0; i < products.length; i++){
+    copyshirt[products[i].sku] = null;
+  }
+  const [sizeShirt, setSize] = useState(copyshirt);
 
   const WelcomeBanner = ({user}) => {
     return (
@@ -234,21 +180,18 @@ const App = () => {
   };
 
   const outmsg = (bool, id) =>{
-    console.log(sizeShirt[id])
-    if (bool){
-      return (
-          <p> Out of Stock </p>
-      );
-    }
-    else{
-      return(
-        <Button key={"cart" + id + count} disabled={sizeShirt[id] === null} onClick={() => cartAdd(id, sizeShirt[id])}> Add to Cart </Button>
-      )
-    }
+    let value = bool ? <p> Out of Stock </p> : 
+          <Button key={"cart" + id + count} disabled={sizeShirt[id] === null} onClick={() => cartAdd(id, sizeShirt[id])}> Add to Cart </Button> 
+    return value
+  };
+
+  const changeSize = (id, pick_size) =>{
+    let tempSize = sizeShirt;
+    tempSize[id] = pick_size;
+    setSize(tempSize);
   };
 
   const ShirtSizeSelector = (id) => {
-    const sizes = ["S", "M", "L", "XL"];
     let size_buttons = []
     let out_of_stock = true
 
@@ -305,58 +248,64 @@ const App = () => {
         <Button.Group hasAddons>
           {size_buttons}
         </Button.Group>
-        <p> {outmsg(out_of_stock, id)} </p>
+        {outmsg(out_of_stock, id)}
       </React.Fragment>
     );
   };
 
-  const ProductList = ( {num_products, products} ) => {
-    let items = [];
-    for (let i = 0; i < num_products; i++){
-      items.push(
-        <Card key={products[i].sku}> 
+  const ProductCard = ( { product } ) => {
+    return (
+      <Card key={product.sku}> 
             <br></br>
             <Card.Image align="center">
-              <img src={'data/products/' + products[i].sku + '_1.jpg'} width="200px" height="400px"/>
+              <img alt="product + id" src={'data/products/' + product.sku + '_1.jpg'} width="200px" height="400px"/>
             </Card.Image>
-            <Card.Header.Title align="center">{products[i].title}</Card.Header.Title> 
+            <Card.Header.Title align="center">{product.title}</Card.Header.Title> 
             <Card.Content>
-              <em>{products[i].style}, {products[i].description}</em>
+              <em>{product.style}, {product.description}</em>
               <br></br>
               <Card.Footer>
                 <b>
                   <br></br>
-                  {products[i].currencyFormat}
-                  {formatPrice(products[i].price)}
+                  {product.currencyFormat}
+                  {formatPrice(product.price)}
                   <br></br>
                 </b>
               </Card.Footer>
               <br></br>
-              {ShirtSizeSelector(products[i].sku)}
+              {ShirtSizeSelector(product.sku)}
               <br></br>
               <br></br>
             </Card.Content> 
-        </Card>
+      </Card>
+    );
+  };
+
+  const ProductList = ( { products } ) => {
+    let items = [];
+    for (let i = 0; i < products.length; i++){
+      items.push(
+       <ProductCard product={products[i]} />
       )
     }
 
     let column = [];
-    for(let i = 0; i < num_products; i = i + 4){
-      if(i + 3 < num_products){
+    for(let i = 0; i < products.length; i = i + 4){
+      if(i + 3 < products.length){
         column.push(
           <Column size="3" key={products[i].sku}>
             {items[i]}{items[i + 1]}{items[i + 2]}{items[i+3]}
           </Column>
         )
       }
-      else if(i + 2 < num_products){
+      else if(i + 2 < products.length){
         column.push(
           <Column size="3" key={products[i].sku}>
             {items[i]}{items[i + 1]}{items[i + 2]}
           </Column>
         )
       }
-      else if (i + 1 < num_products){
+      else if (i + 1 < products.length){
         column.push(
           <Column size="3" key={products[i].sku}>
             {items[i]}{items[i + 1]}
@@ -375,50 +324,152 @@ const App = () => {
     return column
   };
 
+  const cartAdd = (id, size) => {
+    setCount(count + 1);
+    setOpenCart(true);
+    let tempCart = cart;
+    let tempInv = inventory;
+    let inCart = false;
+    for (let i = 0; i < tempCart.length; i++){
+      if (tempCart[i].itemId === (id + size)){
+        if (tempInv[id][size] > 0){
+          tempCart[i].qty += 1;
+          tempInv[id][size] -= 1;
+          setCartTotal(cartTotal + productDict[id].price);
+        }
+        inCart = true;
+        break;
+      }
+    }
+    if (!inCart && tempInv[id][size] > 0){
+      tempCart.push(
+        {itemId: id + size, 
+         qty: 1, 
+         product: productDict[id],
+         buy_size: size
+       })
+      tempInv[id][size] -= 1;
+      setCartTotal(cartTotal + productDict[id].price);
+    }
+    setCart(tempCart);
+    setInventory(tempInv);
+    if(user){
+      const db2 = firebase.database().ref('carts/' + user.uid);
+      const handleData2 = snap => {
+        db.child('carts').child(user.uid).set(tempCart)
+        db2.off('value', handleData2);
+      }
+      db2.on('value', handleData2, error => alert(error));
+    }
+  };
+
+  const cartRemove = (id, size) => {
+    setCount(count + 1);
+    setOpenCart(true);
+    let tempCart = cart;
+    let tempInv = inventory; 
+    for (let i = 0; i < tempCart.length; i++){
+      if (tempCart[i].itemId === (id + size)){
+        tempCart[i].qty -= 1;
+        tempInv[id][size] += 1;
+        break;
+      }
+    }
+
+    tempCart = tempCart.filter(item => item.qty > 0)
+    setCart(tempCart);
+    setCartTotal(cartTotal - productDict[id].price);
+    setInventory(tempInv);
+    if(user){
+      const db2 = firebase.database().ref('carts/' + user.uid);
+      const handleData2 = snap => {
+        db.child('carts').child(user.uid).set(tempCart)
+        db2.off('value', handleData2);
+      }
+      db2.on('value', handleData2, error => alert(error));
+    }
+  };
+
+  const CartItem = ({product, cartItem}) => {
+    return (
+      <Card key={product.sku+sizeShirt[product.sku]} style={{display:'inline-block', width:'100%'}}>
+        <div>
+          <div style={{float:'left'}}>
+            <Card.Image align="left">
+              <img alt="product + id" src={'data/products/' + product.sku + '_1.jpg'} width="100px" height="100px"/>
+            </Card.Image>
+          </div>
+          <div style={{float:'center'}}>
+            <Card.Content style={{align:'right'}}>
+              <b>{product.title}</b>
+              <br></br>
+              <em>{product.style}</em>
+              <br></br>
+              <em>{product.description}</em>
+              <br></br>
+              <div>
+                <div>
+                  <Button key={"plus" + product.sku + cartItem.itemId} disabled={inventory[product.sku][cartItem.buy_size] === 0} onClick={ () => cartAdd(product.sku, cartItem.buy_size) }> + </Button>
+                  <Button key={"minus" + product.sku + cartItem.itemId} onClick={ () => cartRemove(product.sku, cartItem.buy_size) }> - </Button>
+                </div>
+              </div>
+              <div style={{float:'right'}}>
+                  <b>
+                    <br></br>
+                    <p>SIZE: {cartItem.buy_size} </p>
+                    <p>QTY: {cartItem.qty} </p>
+                    <br></br>
+                    {product.currencyFormat}
+                    {formatPrice(product.price)}
+                  </b>
+              </div>
+            </Card.Content>
+          </div>
+        </div>
+      </Card>
+    );
+  };
+
   const CartItems = () => {
     let list = [];
     let products = cart.map(item => item.product)
     for (let i = 0; i < products.length; i++){
       list.push(
-        <Card key={products[i].sku+sizeShirt[products[i].sku]} style={{display:'inline-block', width:'100%'}}>
-          <div>
-            <div style={{float:'left'}}>
-              <Card.Image align="left">
-                <img src={'data/products/' + products[i].sku + '_1.jpg'} width="100px" height="100px"/>
-              </Card.Image>
-            </div>
-            <div style={{float:'center'}}>
-              <Card.Content style={{align:'right'}}>
-                <b>{products[i].title}</b>
-                <br></br>
-                <em>{products[i].style}</em>
-                <br></br>
-                <em>{products[i].description}</em>
-                <br></br>
-                <div>
-                  <div>
-                    <Button key={"plus" + products[i].sku + cart[i].itemId} disabled={inventory[products[i].sku][cart[i].buy_size] == 0} onClick={ () => cartAdd(products[i].sku, cart[i].buy_size) }> + </Button>
-                    <Button key={"minus" + products[i].sku + cart[i].itemId} onClick={ () => cartRemove(products[i].sku, cart[i].buy_size) }> - </Button>
-                  </div>
-                </div>
-                <div style={{float:'right'}}>
-                    <b>
-                      <br></br>
-                      <p>SIZE: {cart[i].buy_size} </p>
-                      <p>QTY: {cart[i].qty} </p>
-                      <br></br>
-                      {products[i].currencyFormat}
-                      {formatPrice(products[i].price)}
-                    </b>
-                </div>
-              </Card.Content>
-            </div>
-          </div>
-        </Card>
+        <CartItem product={products[i]} cartItem={cart[i]} />
       )
     }
     return (
       <Column> {list} </Column>
+    );
+  };
+
+  const ShoppingCart = () => {
+    return (
+      <Container>
+        <Button
+          style={{marginRight:'40px'}}
+          onClick={() => setOpenCart(true)}> <b>Cart </b>
+        </Button>
+        <Drawer
+          width="100%"
+          anchor="right"
+          open={openCart}
+        > 
+          <Delete onClick={() => setOpenCart(false)}></Delete>
+          <Box style={{pading:'20px'}}>
+            <Title> My Cart </Title>
+          </Box>
+          <Box style={{width:'400px'}}>
+            <Column.Group>
+              {CartItems()}
+            </Column.Group>
+          </Box>
+          <Box style={{float:'left'}}>
+            <p> Total: ${formatPrice(cartTotal)} </p>
+            <Button> Checkout </Button>
+          </Box>
+        </Drawer>
+      </Container>
     );
   };
 
@@ -432,8 +483,8 @@ const App = () => {
       <WelcomeBanner user={user} />
       <br></br>
       <Container>
-        <Column.Group multiline="true">
-          {ProductList({num_products, products})}
+        <Column.Group multiline>
+          {ProductList({products})}
         </Column.Group>
       </Container>
     </React.Fragment>
